f3f9af2baa9682836717dc21427869f5003c702c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-12-31 23:45:49 +0530

    remove the jsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import About from './pages/About.jsx'
+import About from './pages/About'
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
