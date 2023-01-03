7cdaca54d703b18e75506f1684585dd3f15f5ac2
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-03 19:22:08 +0530

    add mainlayout and children

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import About from './pages/About'
+import About from "./pages/About"
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import AuthLayout from './layouts/AuthLayout';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+        path: "/",
+        element: <MainLayout/>,
+        children:[
+        {
+            path: "/",
+            element: <HomePage/>,
+        }
+      ]
     },
     {
-        path:'/about',
-        element: <About/>
+        element: <AuthLayout/>,
+        children:[
+            {
+                path: 'login',
+                element: <Login/>
+            }
+        ]
     }
+    
   ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
