8709a19a25ee57aff0e4fd6a510fb52fd546828f
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-04 13:41:56 +0530

    auth layout file

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const AuthLayout = () => {
+  return (
+    <div>
+        <h1>AuthLayout</h1>
+        <Outlet/>
+        
+    </div>
+
+  )
+}
+
+export default AuthLayout
