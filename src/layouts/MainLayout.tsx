67bbee5bfa467d1395bd748a6dd518622c8300cd
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-07 23:57:36 +0530

    add Logo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,9 @@ import {Outlet} from 'react-router-dom'
 const MainLayout = () => {
   return (
     <div>
-        <aside>Sitebar</aside>
+        <aside>
+            <div>LOGO</div>
+        </aside>
         <main>
             <Outlet/>
         </main>
