0ed378d5488470e4dd84631b60120459ca3fa0b3
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-07 23:59:08 +0530

    add table_status

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+
+import { TABLE_STATUS } from '../enums'
+import Table from "../components/Table"
+const tables = [   
+    {
+        id:1,
+        tableNumber:1,
+        status:TABLE_STATUS.AVAILABLE,
+    },
+    {
+        id:2,
+        tableNumber:2,
+        status:TABLE_STATUS.OCCUPIED
+    },
+    {
+        id: 3,
+        tableNumber: 3,
+        status: TABLE_STATUS.OCCUPIED 
+    }
+
+]
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="flex gap-2">
+            {
+                tables.map(table=> (<Table key={table.id} tableNumber={table.tableNumber} status={table.status}/>))
+            }
+    </div>
   )
 }
 
