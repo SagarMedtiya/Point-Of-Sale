6b64e7bc95e12746fe806d5be265da6d72aa9742
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2023-01-09 23:52:00 +0530

    add list of tables and menu

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom'
+import {Link, Outlet} from 'react-router-dom'
 const MainLayout = () => {
   return (
     <div>
         <aside>
             <div>LOGO</div>
+            <ul>
+                <li>
+                    <Link to="/">Tables</Link>
+                </li>
+                <li>
+                    <Link to="/menu">Menu</Link>
+                </li>
+            </ul>
         </aside>
         <main>
             <Outlet/>
